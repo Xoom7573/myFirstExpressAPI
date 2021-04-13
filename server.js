3ac08802d6ff0f9acc899539bb60564c3bd2ba29
Xoom7573 <66717268+Xoom7573@users.noreply.github.com>
2021-04-13 17:34:57 +0200

    update

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ app.listen(5000, ()=>console.log('...'));
 
 var o = {"json":true,"data":[0,1,2,3,4,5]};
 
+app.get('/', (req,res)=>{
+    res.send('My First Express API');
+});
+
 app.get('/api', (req,res)=>{
     res.send(JSON.stringify(o));
 });
